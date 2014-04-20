ccf4231e021a2cc27ce7b6b03478baa19dd2d0ed
Matt Lea <mlea@schematical.com>
2014-04-20 09:08:32 -0500

    njax-orama

--- app/templates/lib/model/schema.js
+++ app/templates/lib/model/schema.js
@@ -12,11 +12,22 @@ module.exports = function(app){
     };
 
     var <%= _model.name.toLowerCase() %>Schema = new Schema(fields);
-        <%= _model.name.toLowerCase() %>Schema.pre('save', function(next){
-            if(!this._id){
-                this._id = new app.mongoose.Types.ObjectId();
-            }
-            return next();
+    <% if(_model.fields.namespace){ %>
+        <%= _model.name.toLowerCase() %>Schema.virtual('uri').get(function(){
+            return '/<%= _model.name.toLowerCase() %>s/' + this.namespace;
         });
+    <% } %>
+    <%= _model.name.toLowerCase() %>Schema.pre('save', function(next){
+        if(!this._id){
+            this._id = new app.mongoose.Types.ObjectId();
+        }
+        return next();
+    });
+
+    if (!<%= _model.name.toLowerCase() %>Schema.options.toObject) <%= _model.name.toLowerCase() %>Schema.options.toObject = {};
+    <%= _model.name.toLowerCase() %>Schema.options.toObject.transform = function (doc, ret, options) {
+        ret.uri = doc.uri;
+    }
+
     return app.mongoose.model('<%= _.capitalize(_model.name) %>', <%= _model.name.toLowerCase() %>Schema);
 }
--- app/templates/lib/routes/model/route.js
+++ app/templates/lib/routes/model/route.js
@@ -1,19 +1,15 @@
 var path = require('path');
 var fs = require('fs');
+var ObjectId = require('mongoose').Types.ObjectId;
 module.exports = function(app, uri){
     if(!uri) uri = '/<%= _model.name.toLowerCase() %>s';
-    <% if(_model.fields.namespace){ %>
+
     app.param('<%= _model.name.toLowerCase() %>', populate)
-    <% } %>
+
     app.get(uri, render_list);
     app.get(uri + '/new', render_edit);
     app.get(uri + '/:<%= _model.name.toLowerCase() %>', render_detail);
-    app.get(
-        uri + '/:<%= _model.name.toLowerCase() %>/edit',
-
-            render_edit
-
-    );
+    app.get(uri + '/:<%= _model.name.toLowerCase() %>/edit',render_edit);
     app.post(
         uri,
         [
@@ -33,19 +29,34 @@ module.exports = function(app, uri){
         ]
     );
 
-    <% if(_model.fields.namespace){ %>
-        function populate(req, res, next, id){
-            console.log("Populating <%= _.capitalize(_model.name) %>: " + id);
-            app.model.<%= _.capitalize(_model.name) %>.findOne({ namespace: id }, function(err, <%= _model.name.toLowerCase() %>){
-                if(err){
-                    return next(err);
-                }
-                console.log("Populatinged! <%= _.capitalize(_model.name) %>: " + id);
-                req.<%= _model.name.toLowerCase() %> = <%= _model.name.toLowerCase() %>;//TODO:Bootstrap
-                return next();
-            })
+
+    function populate(req, res, next, id){
+        var or_condition = []
+
+
+        var checkForHexRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+        if(checkForHexRegExp.test(id)){
+            or_condition.push({ _id:new ObjectId(id) });
         }
-    <% } %>
+        <% if(_model.fields.namespace){ %>
+            or_condition.push({ namespace:id });
+        <% } %>
+        if(or_condition.length == 0){
+            return next();
+        }
+        var query = { $or: or_condition };
+            console.log(query);
+        app.model.<%= _.capitalize(_model.name) %>.findOne(query, function(err, <%= _model.name.toLowerCase() %>){
+            if(err){
+                return next(err);
+            }
+
+            console.log("Populatinged! <%= _.capitalize(_model.name) %>: " + id, <%= _model.name.toLowerCase() %>);
+            res.bootstrap('<%= _model.name.toLowerCase() %>', <%= _model.name.toLowerCase() %>);
+            return next();
+        })
+    }
+
     function render_list(req, res, next){
         app.model.<%= _.capitalize(_model.name) %>.find({}, function(err, <%= _model.name %>){
             if(err) return next(err);
