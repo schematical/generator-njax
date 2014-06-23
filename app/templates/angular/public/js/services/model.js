763e068e162b1cc3de30aace0ad02b9b6d63f89d
Matt Lea <mlea@schematical.com>
2014-06-23 16:39:54 -0500

    disqus can suck my ass

--- app/templates/angular/public/js/services/model.js
+++ app/templates/angular/public/js/services/model.js
@@ -4,7 +4,7 @@ var <%= config.app_name %>_services = angular.module('<%= config.app_name %>.<%=
     [
         '$resource',
         function($resource){
-            return $resource('<%= _model.uri %>/:<%= _model.name %>_id', {}, {
+            return $resource( '//' + njax_bootstrap.api_url + '<%= _model.uri %>/:<%= _model.name %>_id', {}, {
                 query: {
                     method:'GET',
                     params:{
